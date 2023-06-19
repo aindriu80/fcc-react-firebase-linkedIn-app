c834e5c1f6dd9c9b70d538e4a41e3c39c15a4d6a
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-06-19 14:53:52 +0100

    implementing the comments setion

--- src/components/common/LikeButton/LikeButton.jsx
+++ src/components/common/LikeButton/LikeButton.jsx
@@ -8,9 +8,14 @@ import { useState } from 'react'
 const LikeButton = ({ userId, postId }) => {
   const [likesCount, setLikesCount] = useState(0)
   const [liked, setLiked] = useState(false)
+  const [buttonVisible, setButtonVisible] = useState(false)
 
   const handleLike = () => {
     likePost(userId, postId, liked)
+  }
+
+  const handleComment = () => {
+    setOpen(true)
   }
 
   useMemo(() => {
@@ -23,23 +28,38 @@ const LikeButton = ({ userId, postId }) => {
         {likesCount} People like this Post
       </p>
       <hr className="hr-line" />
-      <div className="like-container" onClick={handleLike}>
-        <span className="feed-shared-social-action">
-          {liked ? <AiFillLike color="#378FE9" /> : <AiOutlineLike />}
-          <span className={liked ? 'blue' : 'black'}>Like</span>
-        </span>
-        <span className="feed-shared-social-action">
-          <BiCommentDetail />
-          Comment
-        </span>
-        <span className="feed-shared-social-action">
-          <BiRepost />
-          Repost
-        </span>
-        <span className="feed-shared-social-action">
-          <AiOutlineSend />
-          Send
-        </span>
+      <div className="like-container-main">
+        <div className="social-action" onClick={handleLike}>
+          <span className="feed-shared-social-action">
+            {liked ? <AiFillLike color="#378FE9" /> : <AiOutlineLike />}
+            <span className={liked ? 'blue' : 'black'}>Like</span>
+          </span>
+        </div>
+        <div className="social-action" onClick={handleComment}>
+          <span className="feed-shared-social-action">
+            <BiCommentDetail />
+            Comment
+          </span>
+        </div>
+        <div className="social-action">
+          <span className="feed-shared-social-action">
+            <BiRepost />
+            Repost
+          </span>
+        </div>
+        <div className="social-action">
+          <span className="feed-shared-social-action">
+            <AiOutlineSend />
+            Send
+          </span>
+        </div>
+      </div>
+      <div className="like-comment-component">
+        <input
+          type="text"
+          placeholder="Add a comment"
+          className="comment-input"></input>
+        <button className="submit-comment-button">Post</button>
       </div>
     </>
   )
